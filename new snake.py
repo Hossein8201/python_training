383037819ecc4b02211d807c84855a386605e515
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-04-17 23:03:25 +0330

    first commit

---
+++ new snake.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.locals import *
+from pygame.color import Color
+import math
+import random
+
+pygame.init()
+surface = pygame.display.set_mode([1000,600])
+
+def snake():
+    pass
+
