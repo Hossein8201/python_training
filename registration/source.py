8840ad47b9eff4effe9da01946243ff939ce9f79
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-04-19 16:08:22 +0330

    quera

--- registration/source.py
+++ registration/source.py
@@ -11,3 +11,6 @@ def check_registration_rules(**kwargs):
                         break
                 if(c):  answer.append(i)
     return answer
+
+print(check_registration_rules(username='password', sadegh='He3@lsa', quera='kLS45@l$'))
+print(check_registration_rules(saeed='1234567', ab='afj$L12'))
