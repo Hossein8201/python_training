6496f8a6b31061c1c00786ebf0d7818deed94c80
hossein tatar <161024752+Hossein8201@users.noreply.github.com>
2024-04-19 16:16:27 +0330

    -

--- elevator/calculator.py
+++
@@ -1,9 +0,0 @@
-def calculate_floor(string):
-    floor=0
-    for i in range(4):
-        if(string[i]=='U'):     floor+=1       
-        else:   floor-=1
-    return floor
-
-print(calculate_floor('UUDU'))
-print(calculate_floor('DDDD'))
